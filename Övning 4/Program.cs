a01524edbc45e383804e20216a803d03c6cff4ce
krbu06001 <krbu06001@E-PW05CGQY>
2024-09-12 13:49:20 +0200

    Add project files.

---
+++ Övning 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ändra bakgrundsfärg till mörkröd
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+
+            // Ändra textfärgen till vit 
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Ett testmeddelande");
+
+        }
+    }
+}
+
+    
+    
+
+
+    
